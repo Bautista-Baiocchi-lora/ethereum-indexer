87066373a094b4495111fd693ec78d3fef2b0443
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2022-03-10 17:49:02 -0300

    fix: All port and host to be specified

--- server/src/main.py
+++ server/src/main.py
@@ -1,8 +1,10 @@
 
 #!/usr/bin/env python
+import asyncio
 import logging
 import os
 import sys
+from typing import Optional
 
 from aiohttp import web
 from tartiflette_aiohttp import register_graphql_handlers
@@ -12,17 +14,21 @@ from interfaces.iserver import IServer
 
 class Server(IServer):
 
-    def __init__(self, to_serve: str, port: int, graphiql_debug: bool = False):
+    def __init__(self, to_serve: str, host: Optional[str] = "0.0.0.0", port: Optional[int] = 8080, graphiql_debug: Optional[bool] = False):
         self.to_serve = to_serve
+        self.host = host
         self.graphiql_debug = graphiql_debug
         self.port = port
 
     def start(self) -> None:
-         """@inheritdoc IServer"""
+        """@inheritdoc IServer"""
+        loop = asyncio.get_event_loop()
 
-         web.run_app(
-            register_graphql_handlers(
-                app=web.Application(),
+        # Init aiohttp server
+        app = web.Application()
+
+        register_graphql_handlers(
+                app,
                 engine_sdl=os.path.dirname(os.path.abspath(__file__)) + "/" + self.to_serve + "/sdl",
                 engine_modules=[
                     f"{self.to_serve}.query_resolvers",
@@ -31,7 +37,10 @@ class Server(IServer):
                 executor_http_methods=["POST"],
                 graphiql_enabled=self.graphiql_debug,
             )
-        )
+
+        # Bind aiohttp to asyncio
+        web.run_app(app, host=self.host, port=self.port)
+        return 0
         
 
 def main():
@@ -41,7 +50,7 @@ def main():
         format="%(relativeCreated)6d %(process)d %(message)s",
     )
 
-    server = Server("example_rumble_kong_league", 8080, True)
+    server = Server("example_rumble_kong_league", port=8080, graphiql_debug=True)
     server()
 
     logging.debug("server started")
