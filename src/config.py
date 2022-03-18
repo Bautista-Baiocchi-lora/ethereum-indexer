c4c915a68bd8fca5d5ab26d32b4712ef7fe27412
nazariyv <nazariy@inbox.ru>
2022-03-18 13:58:25 +0000

    chore: config

---
+++ src/config.py
@@ -0,0 +1,103 @@
+class Config:
+    """
+    Each Config instance contains all the required information for end-to-end
+    working of indexer and transformer.
+    """
+
+    def __init__(
+        self, address: str, log_filename: str, transformer_name: str, network_id: int
+    ) -> None:
+        self._address = address
+        self._log_filename = log_filename
+        self._transformer_name = transformer_name
+        self._network_id = network_id
+
+    # Getters for the private attributes
+
+    def get_address(self) -> str:
+        """
+        Getter function for private attribute _address.
+
+        Returns:
+            str: address that is indexed and transformed.
+        """
+        return self._address
+
+    def get_log_filename(self) -> str:
+        """
+        Getter function for private attribute _log_filename.
+
+        Returns:
+            str: name of the log file that will be used to
+            log everything related to the transformer and
+            indexer.
+        """
+        return self._log_filename
+
+    def get_transformer_name(self) -> str:
+        """
+        Getter function for private attribute _transformer_name.
+
+        Returns:
+            str: this is the name of the transformer folder
+            where the logic that transforms the raw transaction
+            data sits.
+        """
+        return self._transformer_name
+
+    def get_network_id(self) -> int:
+        """
+        Getter function for private attribute _network_id.
+
+        Returns:
+            str: this indictes what network the contract is on.
+        """
+        return self._network_id
+
+    # Presets
+
+    @classmethod
+    def example_rumble_kong_league(cls):
+        """
+        Returns instance of an example_rumble_kong_league
+        indexer and transformer. It collects all the raw
+        transactions from rumble kong league collection and
+        transforms them such that you can learn about which
+        address holds what kong NFTs (ids).
+
+        Network: Ethereum Mainnet.
+
+        Returns:
+            _type_: instance of this class with the correct
+            configs.
+        """
+
+        address = "0xEf0182dc0574cd5874494a120750FD222FdB909a"
+        log_filename = "example_rumble_kong_league.log"
+        transformer_name = "example_rumble_kong_league"
+        network_id = 1
+
+        return cls(address, log_filename, transformer_name, network_id)
+
+    @classmethod
+    def rkl_club_auction(cls):
+        """
+        Returns instance of an rkl_club_auction
+        indexer and transformer. It collects all the raw
+        transactions from rkl club mint pass auction
+        contract and transforms them such that you can learn
+        about which address bid what amount in total.
+
+        Network: Ethereum Kovan.
+
+        Returns:
+            _type_: instance of this class with the correct
+            configs.
+        """
+
+        address = "0xa10bEa6303E89225D6fA516594632DddB6FBF3b5"
+        log_filename = "rkl_club_auction.log"
+        transformer_name = "rkl_club_auction"
+        network_id = 42
+
+        return cls(address, log_filename, transformer_name, network_id)
